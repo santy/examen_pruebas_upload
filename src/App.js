4d44b8fe11b0a8813e25afd6679943005d7ed947
Santy Cierco <santidisturbios@gmail.com>
2023-11-02 13:09:33 +0100

    Upload Ejercicio 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Ejercicio3 from './Ejercicio3/Ejercicio3';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Ejercicio3 />
     </div>
   );
 }
---
+++ src/Ejercicio3/Ejercicio3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Ejercicio3() {
+  return (
+    <div><h1>Ejercicio3</h1></div>
+  )
+}
+
+export default Ejercicio3
