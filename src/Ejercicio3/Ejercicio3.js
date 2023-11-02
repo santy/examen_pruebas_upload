a44e0269a2c9c63eebbebd5d77016f1ef7843f65
Santy Cierco <santidisturbios@gmail.com>
2023-11-02 13:28:38 +0100

    Ejercicio 3 Completed

--- src/Ejercicio3/Ejercicio3.js
+++ src/Ejercicio3/Ejercicio3.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Ejercicio3() {
   return (
-    <div><h1>Ejercicio3</h1></div>
+    <div>
+      <h1>Ejercicio3</h1>
+      <p>Completed</p>
+    </div>
   )
 }
 
